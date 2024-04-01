8848daf64d635955ab216a3908e985c42ca333bb
Babaloga <31783107+Babaloga@users.noreply.github.com>
2024-04-01 18:12:25 -0400

    FIXED IT FOR REEAAALL

--- Assets/Scripts/Environment Toys/BackgroundDrift.cs
+++ Assets/Scripts/Environment Toys/BackgroundDrift.cs
@@ -22,6 +22,6 @@ public class BackgroundDrift : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.localRotation = Quaternion.Euler(sPos.x + 0, sPos.y + (DateTime.UtcNow.Ticks/ (50f * 10000000)) * scale, sPos.z + 0);
+        transform.localRotation = Quaternion.Euler(sPos.x + 0, sPos.y + (DateTime.UtcNow.Ticks/ (50f * 10000000f)) * scale, sPos.z + 0);
     }
 }
