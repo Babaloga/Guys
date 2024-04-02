60272e08b06b0b21169521edde83054fdbac8329
Babaloga <31783107+Babaloga@users.noreply.github.com>
2024-04-02 12:38:29 -0400

    Fixed Orbs
    The Orbs
    
    They are fixed
    
    rejoice!

--- Assets/Scripts/Environment Toys/BackgroundDrift.cs
+++ Assets/Scripts/Environment Toys/BackgroundDrift.cs
@@ -7,21 +7,23 @@ using Unity.Netcode;
 public class BackgroundDrift : MonoBehaviour
 {
     MeshRenderer rendered;
-    public float scale = 1;
+    public double scale = 1;
     public float offset = 0;
 
     Vector3 sPos;
+    private double startSeconds;
 
     // Start is called before the first frame update
     void Start()
     {
         sPos = transform.localRotation.eulerAngles;
-        
+        startSeconds = (double)(DateTime.UtcNow.Ticks - new DateTime(2024, 1, 1, 0, 0, 0, DateTimeKind.Utc).Ticks) / (double)10000000;
+        //startSeconds = startSeconds % (360 / scale);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.localRotation = Quaternion.Euler(sPos.x + 0, sPos.y + (DateTime.UtcNow.Ticks/ (50f * 10000000f)) * scale, sPos.z + 0);
+        transform.localRotation = Quaternion.Euler(sPos.x + 0, (float)((sPos.y + ((startSeconds + Time.timeAsDouble) * scale / (double)100))%(double)360), sPos.z + 0);
     }
 }
